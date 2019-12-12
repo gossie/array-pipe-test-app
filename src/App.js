f24f560aaf8076307ae61881db403a41ab538395
André Schreck <gossie@users.noreply.github.com>
2019-12-12 03:03:49 +0100

    typo

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ export default function App() {
 
     return (
         <div>
-            This application was created to quickly compare <a href="https://github.com/gossie/array-pipe">array-pipe</a> implementations with conventional implementations. Check out the documentation to find out more about <a href="https://github.com/gossie/array-pipe">array-pipe</a>.<br/>
-            When you click "Run" an array "Number of elements" many entries will be created. The variable name will just be array. The array consists of string encoded numbers in ascending order, beginning with 1 (something like this: ['1', '2', '3', ..., '1000000']). That array can then be used in your code in the codebox.
+            This application was created to quickly compare <a href="https://github.com/gossie/array-pipe">array-pipe</a> implementations with conventional implementations. Check out the documentation to find out more about <a href="https://github.com/gossie/array-pipe">array-pipe</a>.<br/><br/>
+            When you click "Run" an array with "Number of elements" many entries will be created. The variable name will just be array. The array consists of string encoded numbers in ascending order, beginning with 1 (something like this: ['1', '2', '3', ..., '1000000']). That array can then be used in your code in the codebox.
             <span>
                 <Test defaultCode={left} />
             </span>
